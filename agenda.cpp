a35a32251031c401c58318511ba3d5258733ad19
davide vanoncini <davidevanoncini2003@gmail.com>
2021-09-22 18:18:14 +0200

    going on repl xD

--- agenda.cpp
+++ agenda.cpp
@@ -3,39 +3,73 @@
 #include <string>
 using namespace std;
 
-
-class agenda{
-	private:
-		int numOfUsers; //number of users stored in the agenda
-		string filePath; //path to binary file
-		
-		//check if file in filePath exist (true if it does, false if doesnt)
-		bool checkIfExist(){
-			ifstream ifile;
-		    ifile.open(filePath.c_str());
-		    if(ifile) return true;
-			return false;
-		}
-		
-		//create the file using filePath
-		void createFile(){
-			ofstream outfile(filePath.c_str());
-			outfile.close();
-		}
-	public:
-		//constructor
-		agenda(string path){
-			filePath = path;
-		}
-		
-		//get the number of users in the agenda
-		int getNumOfUsers(){
-			
-		}
-			
-			
+struct user
+{
+	char lastName[20];
+	char name[20];
+	char number[15];
 };
 
-int main(){
+class agenda
+{
+private:
+	int numOfUsers;	 //number of users stored in the agenda
+	string filePath; //path to binary file
+
+	//check if file in filePath exist (true if it does, false if doesnt)
+	bool checkIfExist()
+	{
+		ifstream file;
+		file.open(filePath.c_str());
+		if (file)
+			return true;
+		return false;
+	}
+
+	//create the file using filePath
+	void createFile()
+	{
+		ofstream file(filePath.c_str());
+		file.close();
+	}
+
+	//get the number of users in the agenda
+	int getNumOfUsers()
+	{
+	}
+
+	void printUser(user u)
+	{
+		cout << "numero:  " << string(u.number) << endl;
+		cout << "nome:    " << string(u.name) << endl;
+		cout << "cognome: " << string(u.lastName) << endl;
+	}
+
+public:
+	//constructor
+	agenda(string path)
+	{
+		filePath = path;
+	}
+
+	void getUsers()
+	{
+		user u;
+		for (int i = 0; i < numOfUsers; i++)
+		{
+		}
+	}
+
+	//write in append mode the user on the agenda file
+	void writeUser(user u)
+	{
+		fstream file(filePath.c_str(), ios::app | ios::binary);
+		file.write((char *)&u, sizeof(user));
+		file.close();
+	}
+};
+
+int main()
+{
 	agenda a("agenda.dat");
 }
