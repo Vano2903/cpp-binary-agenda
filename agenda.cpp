812e1400e4dd2e26c166e351ede0019b3e703f0e
Vano2903 <davidevanoncini2003@gmail.com>
2021-09-22 23:12:24 +0200

    switching computer again, working on gui and main crud functions

--- agenda.cpp
+++ agenda.cpp
@@ -5,15 +5,15 @@ using namespace std;
 
 struct user
 {
-	string lastName;
-	string name;
-	string number;
+	char lastName[20];
+	char name[20];
+	char number[12];
 };
 
-class agenda
+class addrBook
 {
 private:
-	int numOfUsers;	 //number of users stored in the agenda
+	int numOfUsers;	 //number of users stored in the addrBook
 	string filePath; //path to binary file
 
 	//check if file in filePath exist (true if it does, false if doesnt)
@@ -27,17 +27,24 @@ private:
 	}
 
 	//create the file using filePath
-	void createFile()
+	bool createFile()
 	{
 		ofstream file(filePath.c_str());
+		if (file.fail())
+		{
+			return false;
+		}
 		file.close();
+		return true;
 	}
 
-	//get the number of users in the agenda
+	//get the number of users in the addrBook
 	int getNumOfUsers()
 	{
+		return 0;
 	}
 
+	//print the user in a formatted way
 	void printUser(user u)
 	{
 		cout << "numero:  " << string(u.number) << endl;
@@ -45,52 +52,148 @@ private:
 		cout << "cognome: " << string(u.lastName) << endl;
 	}
 
+	//clear the terminal
+	void clear()
+	{
+		int ret_val = system("cls");
+		if (!(ret_val == 0))
+		{
+			system("clear");
+		}
+	}
+
+	//promt the title of the application
+	void title()
+	{
+		cout << "addrBook file binari - Vanoncini Davide" << endl;
+	}
+
+	//write in append mode the user on the addrBook file
+	//! dont write in append, check the "white spots" in the file from previous users
+	bool writeUser(user u)
+	{
+		fstream file(filePath.c_str(), ios::app | ios::binary);
+		if (file.fail())
+		{
+			return false;
+		}
+		file.write((char *)&u, sizeof(user));
+		file.close();
+		return true;
+	}
+
 public:
 	//constructor
-	agenda(string path)
+	addrBook(string path)
 	{
 		filePath = path;
 		if (checkIfExist())
+			if (!createFile())
+			{
+				cout << "file not created, error occurred";
+				exit(1);
+			}
+	}
+
+	//print all the users in the addrBook
+
+	//promt on console all the possible functions the user can select,
+	//if showTitle is true the function will promt the title of the application
+	int homeMenu(bool showTitle)
+	{
+		string selectionS = "";
+		bool repeat = false;
+		do
 		{
-			createFile();
+			clear();
+			if (repeat)
+				cout << "puoi selezionare solo 1,2,3,4,5,6." << endl;
+			else
+			{
+				if (showTitle)
+					title();
+				cout << "seleziona un azione:" << endl;
+			}
+			cout << "1] aggiungi contatto" << endl;
+			cout << "2] cerca un contatto" << endl;
+			cout << "3] visualizza la rubrica" << endl;
+			cout << "4] modifica un contatto" << endl;
+			cout << "5] elimina un contatto" << endl;
+			cout << "6] esci dal programma" << endl;
+			getline(cin, selectionS);
+			repeat = true;
+		} while (selectionS != "1" && selectionS != "2" && selectionS != "3" && selectionS != "4" && selectionS != "5" && selectionS != "6");
+		return stoi(selectionS);
+	}
+
+	void functionHandler(int selected)
+	{
+		if (selected < 1 || selected > 6)
+			return;
+		switch (selected)
+		{
+		case 1:
+			addUser();
+		case 2:
+			// searchUser();
+		case 3:
+			printUsers();
+		case 4:
+			// updateUser();
+		case 5:
+			// deleteUser();
+		case 6:
+			exit(1);
 		}
 	}
 
-	void getUsers()
+	void printUsers()
 	{
 		user u;
-		for (int i = 0; i < numOfUsers; i++)
+
+		fstream file(filePath.c_str(), ios::in | ios::binary); //apre il file binario in LETTURA
+															   // fileBinario.seekg((pos - 1) * sizeof(int), ios::beg);		  //si posiziona per leggere il centesimo intero
+															   // fileBinario.read((char *)&n, sizeof(n));					  //legge il centesimo intero
+															   // fileBinario.close();
+		while (file.good())
 		{
+			file.read((char *)&u, sizeof(user));
+			printUser(u);
 		}
 	}
 
-	//write in append mode the user on the agenda file
-	void writeUser(user u)
+	//get user's info and add the user to the file
+	bool addUser()
 	{
-		fstream file(filePath.c_str(), ios::app | ios::binary);
-		file.write((char *)&u, sizeof(user));
-		file.close();
-	}
-
-	void title()
-	{
-		cout << "agenda file binari - Vanoncini Davide"
-	}
-
-	int homeMenu()
-	{
-		string selectionS;
-		cout << "seleziona un azione:" << endl;
-		cout << "1] aggiungi contatto" << endl;
-		cout << "2] cerca un contatto" << endl;
-		cout << "3] visualizza la rubirca" << endl;
-		cout << "4] modifica un contatto" << endl;
-		cout << "5] elimina un contatto" << endl;
-		cout << "6] esci dal programma" << endl;
+		user u;
+		string name, lastName, number = "";
+		clear();
+		do
+		{
+			cout << "AGGIUNGI UN NUOVO UTENTE" << endl;
+			cout << "nome (max 20 caratteri)               > ";
+			cin >> name;
+			cout << "cognome (max 20 caratteri)            > ";
+			cin >> lastName;
+			cout << "numero di telefono (max 20 caratteri) > ";
+			cin >> number;
+		} while (name.length() > 20 && lastName.length() > 20 && number.length() > 12);
+		if (writeUser(u))
+		{
+			cout << "added successfully" << endl;
+			return false;
+		}
+		cout << "error adding to file" << endl;
+		return true;
 	}
 };
 
 int main()
 {
-	agenda a("agenda.dat");
+	addrBook a("rubrica.dat");
+	do
+	{
+		int selected = a.homeMenu(true);
+		a.functionHandler(selected);
+	} while (true);
 }
