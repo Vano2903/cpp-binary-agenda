420a5749d88e54919eb7bef4d86b17e540102902
davide vanoncini <davidevanoncini2003@gmail.com>
2021-09-22 17:23:35 +0200

    function to create file and check if exist (working)

--- agenda.cpp
+++ agenda.cpp
@@ -6,39 +6,36 @@ using namespace std;
 
 class agenda{
 	private:
-		int numOfUsers;
+		int numOfUsers; //number of users stored in the agenda
+		string filePath; //path to binary file
 		
-		string filePath  = "agenda.txt";
-		
-	public:
-//		agenda(string path){
-//			filePath = path;
-//		}
-		void createFile(){
-			ofstream outfile ("ciao.txt");
-			outfile << "my text here!" << endl;
-			outfile.close();
-		}
+		//check if file in filePath exist (true if it does, false if doesnt)
 		bool checkIfExist(){
 			ifstream ifile;
-		    ifile.open((char*) &filePath);
-		    if(ifile) {
-		   		cout << "trovato";
-		   		return true;
-		    } 
-		    cout << "non trovato";
+		    ifile.open(filePath.c_str());
+		    if(ifile) return true;
 			return false;
 		}
 		
+		//create the file using filePath
+		void createFile(){
+			ofstream outfile(filePath.c_str());
+			outfile.close();
+		}
+	public:
+		//constructor
+		agenda(string path){
+			filePath = path;
+		}
+		
+		//get the number of users in the agenda
 		int getNumOfUsers(){
 			
 		}
 			
+			
 };
 
 int main(){
-	agenda a; //("agenda.txt")
-	a.createFile();
-	cout << a.checkIfExist();
-//cout << __cplusplus;
+	agenda a("agenda.dat");
 }
