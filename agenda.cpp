961802859aa9b0bfeb5d992738fe65847fc62b1b
Vano2903 <davidevanoncini2003@gmail.com>
2021-09-29 12:12:10 +0200

    agenda not really working, should check out test.cpp

--- agenda.cpp
+++ agenda.cpp
@@ -3,197 +3,182 @@
 #include <string>
 using namespace std;
 
-struct user
-{
-	char lastName[20];
-	char name[20];
-	char number[12];
+struct user {
+    string lastName;
+    string name;
+    string number;
 };
 
-class addrBook
-{
+class addrBook {
 private:
-	int numOfUsers;	 //number of users stored in the addrBook
-	string filePath; //path to binary file
+    int numOfUsers;  //number of users stored in the addrBook
+    string filePath; //path to binary file
 
-	//check if file in filePath exist (true if it does, false if doesnt)
-	bool checkIfExist()
-	{
-		ifstream file;
-		file.open(filePath.c_str());
-		if (file)
-			return true;
-		return false;
-	}
+    //check if file in filePath exist (true if it does, false if doesnt)
+    bool checkIfExist(){
+        ifstream file;
+        file.open(filePath.c_str());
+        if (file)
+            return true;
+        return false;
+    }
 
-	//create the file using filePath
-	bool createFile()
-	{
-		ofstream file(filePath.c_str());
+	bool createFile(){
+		fstream file(filePath.c_str(), ios::out);
 		if (file.fail())
-		{
 			return false;
-		}
 		file.close();
 		return true;
 	}
 
-	//get the number of users in the addrBook
-	int getNumOfUsers()
-	{
-		return 0;
-	}
+    //get the number of users in the addrBook
+    int getNumOfUsers() {
+        return 0;
+    }
 
-	//print the user in a formatted way
-	void printUser(user u)
-	{
-		cout << "numero:  " << string(u.number) << endl;
-		cout << "nome:    " << string(u.name) << endl;
-		cout << "cognome: " << string(u.lastName) << endl;
-	}
+    //print the user in a formatted way
+    void printUser(user u) {
+        cout << "numero:  " << u.number << endl;
+        cout << "nome:    " << u.name << endl;
+        cout << "cognome: " << u.lastName << endl;
+    }
 
-	//clear the terminal
-	void clear()
-	{
-		int ret_val = system("cls");
-		if (!(ret_val == 0))
-		{
-			system("clear");
-		}
-	}
+    //clear the terminal
+    void clear() {
+        int ret_val = system("cls");
+        if (!(ret_val == 0))
+        {
+            system("clear");
+        }
+    }
 
-	//promt the title of the application
-	void title()
-	{
-		cout << "addrBook file binari - Vanoncini Davide" << endl;
-	}
+    //promt the title of the application
+    void title() {
+        cout << "addrBook file binari - Vanoncini Davide" << endl;
+    }
 
-	//write in append mode the user on the addrBook file
-	//! dont write in append, check the "white spots" in the file from previous users
-	bool writeUser(user u)
-	{
-		fstream file(filePath.c_str(), ios::app | ios::binary);
-		if (file.fail())
-		{
-			return false;
-		}
-		file.write((char *)&u, sizeof(user));
-		file.close();
-		return true;
-	}
+    //write in append mode the user on the addrBook file
+    //! dont write in append, check the "white spots" in the file from previous users
+    bool writeUser(user u) {
+        fstream file(filePath.c_str(), ios::app | ios::binary);
+        if (file.fail())
+        {
+            return false;
+        }
+        file.write((char*) &u, sizeof(user));
+        file.close();
+        return true;
+    }
 
 public:
-	//constructor
-	addrBook(string path)
-	{
-		filePath = path;
-		if (checkIfExist())
-			if (!createFile())
-			{
-				cout << "file not created, error occurred";
-				exit(1);
-			}
-	}
+    //constructor
+    addrBook(string path) {
+        filePath = path;
+        if (checkIfExist())
+            if (!createFile())
+            {
+                cout << "file not created, error occurred";
+                exit(1);
+            }
+    }
 
-	//print all the users in the addrBook
+    //promt on console all the possible functions the user can select,
+    //if showTitle is true the function will promt the title of the application
+    int homeMenu(bool showTitle) {
+        string selectionS = "";
+        bool repeat = false;
+        do {
+            clear();
+            if (repeat)
+                cout << "puoi selezionare solo 1,2,3,4,5,6." << endl;
+            else {
+                if (showTitle)
+                    title();
+                cout << "seleziona un azione:" << endl;
+            }
+            cout << "1] aggiungi contatto" << endl;
+            cout << "2] cerca un contatto" << endl;
+            cout << "3] visualizza la rubrica" << endl;
+            cout << "4] modifica un contatto" << endl;
+            cout << "5] elimina un contatto" << endl;
+            cout << "6] esci dal programma" << endl;
+            getline(cin, selectionS);
+            repeat = true;
+        } while (selectionS != "1" && selectionS != "2" && selectionS != "3" && selectionS != "4" && selectionS != "5" && selectionS != "6");
+        return stoi(selectionS);
+    }
 
-	//promt on console all the possible functions the user can select,
-	//if showTitle is true the function will promt the title of the application
-	int homeMenu(bool showTitle)
-	{
-		string selectionS = "";
-		bool repeat = false;
-		do
-		{
-			clear();
-			if (repeat)
-				cout << "puoi selezionare solo 1,2,3,4,5,6." << endl;
-			else
-			{
-				if (showTitle)
-					title();
-				cout << "seleziona un azione:" << endl;
-			}
-			cout << "1] aggiungi contatto" << endl;
-			cout << "2] cerca un contatto" << endl;
-			cout << "3] visualizza la rubrica" << endl;
-			cout << "4] modifica un contatto" << endl;
-			cout << "5] elimina un contatto" << endl;
-			cout << "6] esci dal programma" << endl;
-			getline(cin, selectionS);
-			repeat = true;
-		} while (selectionS != "1" && selectionS != "2" && selectionS != "3" && selectionS != "4" && selectionS != "5" && selectionS != "6");
-		return stoi(selectionS);
-	}
+    //print all the users in the addrBook
+    void functionHandler(int selected) {
+        if (selected < 1 || selected > 6)
+            return;
+        switch (selected)
+        {
+        case 1:
+            addUser();
+        case 2:
+            // searchUser();
+			printUser(getUser(1));
+        case 3:
+            printUsers();
+        case 4:
+            // updateUser();
+        case 5:
+            // deleteUser();
+        case 6:
+            exit(1);
+        }
+    }
 
-	void functionHandler(int selected)
-	{
-		if (selected < 1 || selected > 6)
-			return;
-		switch (selected)
-		{
-		case 1:
-			addUser();
-		case 2:
-			// searchUser();
-		case 3:
-			printUsers();
-		case 4:
-			// updateUser();
-		case 5:
-			// deleteUser();
-		case 6:
-			exit(1);
-		}
-	}
+    void printUsers() {
+        user u;
+        fstream file(filePath.c_str(), ios::in | ios::binary); //apre il file binario in LETTURA
+        if(!file) {
+            cout << "Cannot open file!" << endl;
+            return;
+        }
+        while (file.good()) {
+            file.read((char *)&u, sizeof(user));
+            printUser(u);
+        }
+    }
 
-	void printUsers()
-	{
+	user getUser(int index){
 		user u;
-
-		fstream file(filePath.c_str(), ios::in | ios::binary); //apre il file binario in LETTURA
-															   // fileBinario.seekg((pos - 1) * sizeof(int), ios::beg);		  //si posiziona per leggere il centesimo intero
-															   // fileBinario.read((char *)&n, sizeof(n));					  //legge il centesimo intero
-															   // fileBinario.close();
-		while (file.good())
-		{
-			file.read((char *)&u, sizeof(user));
-			printUser(u);
-		}
+    	fstream file(filePath.c_str(), ios::in|ios::binary); //apre il file binario in LETTURA
+		file.seekg((index-1)*sizeof(user), ios::beg);
+		file.read((char*) &u, sizeof(user)); //memorizza i 4 byte (del numero intero) letti dal file binario nella variabile n
+		printUser(u);
+		return u;
 	}
 
-	//get user's info and add the user to the file
-	bool addUser()
-	{
-		user u;
-		string name, lastName, number = "";
-		clear();
-		do
-		{
-			cout << "AGGIUNGI UN NUOVO UTENTE" << endl;
-			cout << "nome (max 20 caratteri)               > ";
-			cin >> name;
-			cout << "cognome (max 20 caratteri)            > ";
-			cin >> lastName;
-			cout << "numero di telefono (max 20 caratteri) > ";
-			cin >> number;
-		} while (name.length() > 20 && lastName.length() > 20 && number.length() > 12);
-		if (writeUser(u))
-		{
-			cout << "added successfully" << endl;
-			return false;
-		}
-		cout << "error adding to file" << endl;
-		return true;
-	}
+    //get user's info and add the user to the file
+    bool addUser() {
+        user u;
+        clear();
+		cout << "AGGIUNGI UN NUOVO UTENTE" << endl;
+		cout << "nome (max 20 caratteri)               > ";
+		cin >> u.name;
+		cout << "cognome (max 20 caratteri)            > ";
+		cin >> u.lastName;
+		cout << "numero di telefono (max 12 caratteri) > ";
+		cin >> u.number;
+
+        if (writeUser(u)) {
+            cout << "added successfully" << endl;
+            return false;
+        }
+        cout << "error adding to file" << endl;
+        return true;
+    }
 };
 
 int main()
 {
-	addrBook a("rubrica.dat");
-	do
-	{
-		int selected = a.homeMenu(true);
-		a.functionHandler(selected);
-	} while (true);
+    addrBook a("rubrica.dat");
+    do
+    {
+        int selected = a.homeMenu(true);
+        a.functionHandler(selected);
+    } while (true);
 }
--- test.cpp
+++ test.cpp
@@ -1,6 +1,91 @@
-#include <iostream>
+#include<iostream>
+#include<fstream>
+#include<string>
+#include<cstring>
+using namespace std;
+
+struct user {
+    char lastName[20];
+    char name[20];
+    char number[12];
+};
+
+// struct user {
+//     string lastName;
+//     string name;
+//     string number;
+// };
+
+//print the user in a formatted way
+void printUser(user u) {
+    cout << "numero:  " << string(u.number) << endl;
+    cout << "nome:    " << string(u.name) << endl;
+    cout << "cognome: " << string(u.lastName) << endl;
+}
+
+void saveUser(user u){
+    fstream file("test.dat", ios::app | ios::binary);
+    file.write((char*) &u, sizeof(user));
+    file.close();
+}
+
+user getUser(int index){
+    user a;
+    fstream fileBinario("test.dat", ios::in|ios::binary); //apre il file binario in LETTURA
+    fileBinario.seekg(((index)*sizeof(user))+ sizeof(int) , ios::beg); //+ sizeof(int
+    fileBinario.read((char*) &a, sizeof(user)); //memorizza i 4 byte (del numero intero) letti dal file binario nella variabile n
+    fileBinario.close();
+    return a;
+}
+
+void saveNumberOfUsers(int numUsers){
+    fstream file("test.dat", ios::out | ios::binary);
+    file.seekg(0, ios::beg);
+    file.write((char*) &numUsers, sizeof(int));
+    file.close();
+}
+
+int getNumOfUsers(){
+    int numUsers = 0;
+    fstream file("test.dat", ios::in | ios::binary);
+    file.seekg(0, ios::beg);
+    file.read((char*) &numUsers, sizeof(int));
+    file.close();
+    return numUsers;
+}
 
 int main(){
-while(true){
-std::cout<<"LOAF ";
-}}
+    user u[3];
+
+    strncpy(u[0].name, "vano", sizeof(u[0].name - 1));
+    strncpy(u[0].lastName, "vano", sizeof(u[0].lastName - 1));
+    strncpy(u[0].number, "vano", sizeof(u[0].number - 1));
+
+    strncpy(u[1].name, "vano", sizeof(u[1].name - 1));
+    strncpy(u[1].lastName, "vanaa", sizeof(u[1].lastName - 1));
+    strncpy(u[1].number, "2dfsdf", sizeof(u[1].number - 1));
+
+    strncpy(u[2].name, "vano", sizeof(u[2].name - 1));
+    strncpy(u[2].lastName, "vano", sizeof(u[2].lastName - 1));
+    strncpy(u[2].number, "3333", sizeof(u[2].number - 1));
+
+    
+
+    saveNumberOfUsers(3);
+    cout << "num of users: " << getNumOfUsers() << endl;
+
+
+saveUser(u[0]);
+    saveUser(u[1]);
+    saveUser(u[2]);
+
+    printUser(getUser(1));
+    
+
+        saveNumberOfUsers(4);
+    cout << "num of users: " << getNumOfUsers() << endl;
+
+
+    printUser(getUser(1));
+    return 0;
+}
